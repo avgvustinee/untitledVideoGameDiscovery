d056406f9f5e5c0465047157d4508ce9e6ce6823
codewithsyre <augustineshoks7@gmail.com>
2024-11-10 11:31:20 +0200

    Build Game Attribute and Game Trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,9 @@
-import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
+import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -13,6 +15,8 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };

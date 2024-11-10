c0a537f32b245563f960d8909c9d23b1df7cb6bf
codewithsyre <augustineshoks7@gmail.com>
2024-11-10 11:57:35 +0200

    Fix the NavBar search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/rawgg.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image borderRadius={20}  src={logo} boxSize="60px" />
+      <Link to='/'>
+      <Image borderRadius={20}  src={logo} boxSize="60px" objectFit='cover'/>
+      </Link>
+  
       <SearchInput  />
       <ColorModeSwitch />
     </HStack>
